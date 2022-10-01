44a4ab12d19bea493b622b5329e484a27295d1c8
khanyousafzaideveloper <khanyousafzaideveloper@gmail.com>
2022-10-01 18:46:54 +0500

    reverse string completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,8 +1,17 @@
 const reverseString = function(words) {
-    return
-};
+    return (words.split("").reverse().join(""))
+}
 
 reverseString("hello");
+reverseString("hello there");
+reverseString('123! abc!');
+reverseString('');
 
 // Do not edit below this line
 module.exports = reverseString;
+
+//var str = "hello";
+//console.log(str.split("").reverse().join(""))
+//for(let x=str.length() -1; x>0; x--)
+//{
+    
