4196fb1d715f48d9c60e5c1f82f29b52e0bbb6e0
khanyousafzaideveloper <khanyousafzaideveloper@gmail.com>
2022-10-01 09:43:13 +0500

    passed.!

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,22 @@
-const repeatString = function() {
-
+const repeatString = function(string, number) {
+    if(number<0)
+    {
+        return "ERROR";
+    }
+    else{
+        return (string.repeat(number));
+    }
 };
+
+repeatString("hey", 3);
+repeatString("hey", 0);
+repeatString("hey", 1);
+repeatString("hey", 0);
+repeatString("hey", -1);
+const num = Math.floor(Math.random() * 1000)
+repeatString("hey", num);
+repeatString(" ", 10);
+
 
 // Do not edit below this line
 module.exports = repeatString;
